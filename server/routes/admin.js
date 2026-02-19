edc3b067d22ba3682f70739168ad9c441eaec2c3
muhammedanasmp-h <muhammedanasmp1012@gmail.com>
2026-02-19 14:07:51 +0530

    Delete server directory

--- server/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-router.post('/login', adminController.login);
-router.get('/status', adminController.checkStatus);
-router.get('/logout', adminController.logout);
-
-module.exports = router;

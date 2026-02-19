75202cf3b8c4a09a03f25f022997d43ddb041cf3
muhammedanasmp-h <muhammedanasmp1012@gmail.com>
2026-02-19 13:31:47 +0530

    Delete server directory

--- server/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = (req, res, next) => {
-    if (req.session && req.session.isAdmin) {
-        return next();
-    }
-    res.status(401).json({ message: 'Unauthorized: Admin access required' });
-};
